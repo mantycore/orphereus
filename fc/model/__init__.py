8b0fe099af9c4f9806874156aa81c78f8e496369
RusAnon <RusAnon@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-06-12 09:20:54 +0000

    Finished posts table, added piclist table
    git-svn-id: svn://localhost/orphereus/trunk@15 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/model/__init__.py
+++ fc/model/__init__.py
@@ -6,11 +6,25 @@ def init_model(engine):
     meta.engine = engine
     meta.Session = orm.scoped_session(sm)
 
+t_piclist = sa.Table("piclist", meta.metadata,
+    sa.Column("id"       , sa.types.Integer, primary_key=True),
+    sa.Column("path"     , sa.types.String(255), nullable=False),
+    sa.Column("thumpath" , sa.types.String(255), nullable=False),
+    sa.Column("width"    , sa.types.Integer, nullable=False),
+    sa.Column("height"   , sa.types.Integer, nullable=False),
+    sa.Column("spoiler"  , sa.types.Boolean, nullable=True)
+    )
+
 t_posts = sa.Table("posts", meta.metadata,
-    sa.Column("id", sa.types.Integer, primary_key=True),
-    sa.Column("parentid", sa.types.Integer, nullable=False),
-    sa.Column("message", sa.types.UnicodeText, nullable=True),
-    sa.Column("date", sa.types.Date, nullable=False),
+    sa.Column("id"       , sa.types.Integer, primary_key=True),
+    sa.Column("parentid" , sa.types.Integer, nullable=False),
+    sa.Column("message"  , sa.types.UnicodeText, nullable=True),
+    sa.Column("title"    , sa.types.UnicodeText, nullable=True),
+    sa.Column("sage"     , sa.types.Boolean, nullable=True),
+    sa.Column("sign"     , sa.types.String(32), nullable=True),
+    sa.Column("uid_number",sa.types.Integer,nullable=True),
+    sa.Column("picid"    , sa.types.Integer, sa.ForeignKey('piclist.id')),
+    sa.Column("date"     , sa.types.Date, nullable=False),
     sa.Column("last_date", sa.types.Date, nullable=True)
     )
 
@@ -25,6 +39,9 @@ t_post_tags = sa.Table("post_tags", meta.metadata,
     sa.Column('tag_id', sa.types.Integer, sa.ForeignKey('tags.id'))
     )
 
+class Picture(object):
+    pass
+
 class Post(object):
     pass
 
@@ -32,9 +49,11 @@ class Tag(object):
     def __init__(self, tag):
         self.tag = tag
 
+orm.mapper(Picture, t_piclist)
 orm.mapper(Tag, t_tags, properties = {
     'posts' : orm.relation(Post, secondary = t_post_tags)
     })
 orm.mapper(Post, t_posts, properties = {
-    'tags' : orm.relation(Tag, secondary = t_post_tags)
+    'tags' : orm.relation(Tag, secondary = t_post_tags),
+    'piclist': orm.relation(Picture)
     })

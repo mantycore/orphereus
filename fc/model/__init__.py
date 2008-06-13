b51b71ff81725517ec392ce796990b550571339d
RusAnon <RusAnon@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-06-13 02:39:59 +0000

    Added extlist table, edited some other tables
    git-svn-id: svn://localhost/orphereus/trunk@20 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/model/__init__.py
+++ fc/model/__init__.py
@@ -6,6 +6,15 @@ def init_model(engine):
     meta.engine = engine
     meta.Session = orm.scoped_session(sm)
 
+t_extlist = sa.Table("extlist", meta.metadata,
+    sa.Column("id"       , sa.types.Integer, primary_key=True),
+    sa.Column("path"     , sa.types.String(255), nullable=False),
+    sa.Column("thwidth"  , sa.types.Integer, nullable=False),
+    sa.Column("thheight" , sa.types.Integer, nullable=False),
+    sa.Column("ext"      , sa.types.String(16), nullable=False),
+    sa.Column("type"     , sa.types.String(16), nullable=False)
+    )
+
 t_piclist = sa.Table("piclist", meta.metadata,
     sa.Column("id"       , sa.types.Integer, primary_key=True),
     sa.Column("path"     , sa.types.String(255), nullable=False),
@@ -14,6 +23,8 @@ t_piclist = sa.Table("piclist", meta.metadata,
     sa.Column("height"   , sa.types.Integer, nullable=False),    
     sa.Column("thwidth"  , sa.types.Integer, nullable=False),
     sa.Column("thheight" , sa.types.Integer, nullable=False),
+    sa.Column("size"     , sa.types.Integer, nullable=False),
+    sa.Column("extid"    , sa.types.Integer, sa.ForeignKey('extlist.id')), 
     sa.Column("spoiler"  , sa.types.Boolean, nullable=True)
     )
 
@@ -36,10 +47,14 @@ t_tags = sa.Table("tags", meta.metadata,
     )
 
 t_post_tags = sa.Table("post_tags", meta.metadata,
-    sa.Column('id', sa.types.Integer, primary_key=True),
+    #sa.Column('id', sa.types.Integer, primary_key=True),
     sa.Column('post_id', sa.types.Integer, sa.ForeignKey('posts.id')),
-    sa.Column('tag_id', sa.types.Integer, sa.ForeignKey('tags.id'))
+    sa.Column('tag_id', sa.types.Integer, sa.ForeignKey('tags.id')),
+    sa.Column('is_main', sa.types.Boolean, nullable=True)
     )
+
+class Extension(object):
+    pass
 
 class Picture(object):
     pass
@@ -51,7 +66,10 @@ class Tag(object):
     def __init__(self, tag):
         self.tag = tag
 
-orm.mapper(Picture, t_piclist)
+orm.mapper(Extension, t_extlist)
+orm.mapper(Picture, t_piclist, properties = {
+    'extlist' : orm.relation(Extension)
+    })
 orm.mapper(Tag, t_tags, properties = {
     'posts' : orm.relation(Post, secondary = t_post_tags)
     })

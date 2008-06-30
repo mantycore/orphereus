03fc9ff481ad0b5923640cb91f34f21c0dc61848
RusAnon <RusAnon@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-06-30 18:01:14 +0000

    Admin area - creating/editing boards
    git-svn-id: svn://localhost/orphereus/trunk@135 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/controllers/fca.py
+++ fc/controllers/fca.py
@@ -50,47 +50,63 @@ class FcaController(BaseController):
         return render('/wakaba.adminIndex.mako')
     def manageBoards(self):
         c.boardName = 'Boards management'
+        boards = meta.Session.query(Tag).join('options').order_by(TagOptions.persistent.desc()).order_by(TagOptions.section_id.asc()).all()
+        c.boards = []
+        section_id = 0
+        section = []
+        for b in boards:
+            if not section_id:
+                section_id = b.options.section_id
+                section = []
+            if section_id != b.options.section_id:
+                c.boards.append(section)
+                section_id = b.options.section_id
+                section = []
+            section.append(b.tag)
+        if section:
+            c.boards.append(section)        
         return render('/wakaba.manageBoards.mako')
     def editBoard(self,tag):
         c.boardName = 'Edit board'
         c.message = ''
         c.tag = meta.Session.query(Tag).options(eagerload('options')).filter(Tag.tag==tag).first()
         if not c.tag:
-        	c.tag = Tag()
-        	c.tag.options = TagOptions()
-			c.tag.options.comment = ''
-			c.tag.options.section_id = 0
-			c.tag.options.persistent = False
-			c.tag.options.imageless_thread = True
-			c.tag.options.imageless_post = True
-			c.tag.options.images = True
-			c.tag.options.max_fsize = 3145728
-			c.tag.options.min_size = 0
-			c.tag.options.thumb_size = 250
+            c.tag = Tag()
+            c.tag.options = TagOptions()
+            c.tag.options.comment = ''
+            c.tag.options.section_id = 0
+            c.tag.options.persistent = False
+            c.tag.options.imageless_thread = True
+            c.tag.options.imageless_post = True
+            c.tag.options.images = True
+            c.tag.options.max_fsize = 3145728
+            c.tag.options.min_size = 0
+            c.tag.options.thumb_size = 250
         if request.POST.get('tag',False):
-        	newtag = request.POST.get('tag',False)
-        	newtagre = re.compile(r"""([^,@~\+\-\&\s\/\\\(\)<>'"%\d][^,@~\+\-\&\s\/\\\(\)<>'"%]*)""").match(newtag)
-        	if newtagre:
-        		newtag = newtagre.groups()[0]
-        		newtagRecord = meta.Session.query(Tag).options(eagerload('options')).filter(Tag.tag==newtag).first()
-        		if not newtagRecord or newtagRecord.id == c.tag.id:
-        			c.tag.tag = newtag
-        			c.tag.options.comment = request.POST.get('comment','')
-        			c.tag.options.section_id = request.POST.get('section_id',0)
-        			c.tag.options.persistent = request.POST.get('persistent',False)
-        			c.tag.options.imageless_thread = request.POST.get('imageless_thread',True)
-        			c.tag.options.imageless_post = request.POST.get('imageless_post',True)
-        			c.tag.options.images = request.POST.get('images',True)
-        			c.tag.options.max_fsize = request.POST.get('max_fsize',3145728)
-        			c.tag.options.min_size = request.POST.get('min_size',0)
-        			c.tag.options.thumb_size = request.POST.get('thumb_size',250)
-        			if not c.tag.id:
-        				meta.Session.save(c.tag)
-        			meta.Session.commit()
-        		else:
-        			c.message = _("Board %s already exists!") % newtag
-        	else:
-        		c.message = _("Board name should be non-empty and should contain only valid symbols")
+            newtag = request.POST.get('tag',False)
+            newtagre = re.compile(r"""([^,@~\+\-\&\s\/\\\(\)<>'"%\d][^,@~\+\-\&\s\/\\\(\)<>'"%]*)""").match(newtag)
+            if newtagre:
+                newtag = newtagre.groups()[0]
+                newtagRecord = meta.Session.query(Tag).options(eagerload('options')).filter(Tag.tag==newtag).first()
+                if not newtagRecord or newtagRecord.id == c.tag.id:
+                    c.tag.tag = newtag
+                    c.tag.options.comment = request.POST.get('comment','')
+                    c.tag.options.section_id = request.POST.get('section_id',0)
+                    c.tag.options.persistent = request.POST.get('persistent',False)
+                    c.tag.options.imageless_thread = request.POST.get('imageless_thread',True)
+                    c.tag.options.imageless_post = request.POST.get('imageless_post',True)
+                    c.tag.options.images = request.POST.get('images',True)
+                    c.tag.options.max_fsize = request.POST.get('max_fsize',3145728)
+                    c.tag.options.min_size = request.POST.get('min_size',0)
+                    c.tag.options.thumb_size = request.POST.get('thumb_size',250)
+                    if not c.tag.id:
+                        meta.Session.save(c.tag)
+                    meta.Session.commit()
+                    c.message = _("Updated board")
+                else:
+                    c.message = _("Board %s already exists!") % newtag
+            else:
+                c.message = _("Board name should be non-empty and should contain only valid symbols")
         return render('/wakaba.editBoard.mako')
     def manageUsers(self):
         c.boardName = 'Users management'
--- fc/templates/wakaba.manageBoards.mako
+++ fc/templates/wakaba.manageBoards.mako
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
 <%inherit file="wakaba.admin.mako" />
+
+<a href="/holySynod/editBoard">${_('Create new board')}</a>
+<hr />
+%for s in c.boards:
+    %for b in s:
+        <a href="/holySynod/editBoard/${b.tag}">/${b.tag}/</a>
+    %endfor
+    <hr />
+%endfor

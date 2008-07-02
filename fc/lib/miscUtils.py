d3f188e3eb0cf6e704ca793ede233089d395956b
rusanon <rusanon@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-07-02 21:18:49 +0000

    Some fixes
    git-svn-id: svn://localhost/orphereus/trunk@178 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/lib/miscUtils.py
+++ fc/lib/miscUtils.py
@@ -1,2 +1,2 @@
-def filterText(text)
-    return text.replace('<','&lt;').replace('>','&gt;').replace("'",'&#39;').replace('"','&quot;').replace('(c)','&copy;').replace('--','&#150;').replace('---','&#151;').replace('(tm)','&#153;')
+def filterText(text):
+    return text.replace('<','&lt;').replace('>','&gt;').replace("'",'&#39;').replace('"','&quot;').replace('(c)','&copy;').replace('---','&#151;').replace('--','&#8722;').replace('(tm)','&#153;').replace('...','&#8230;')

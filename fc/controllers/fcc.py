c901311bd5e3fe20c315dfecc527f61eccd7c072
rusanon <rusanon@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-07-10 04:16:25 +0000

    Fix for length limit
    git-svn-id: svn://localhost/orphereus/trunk@249 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/controllers/fcc.py
+++ fc/controllers/fcc.py
@@ -417,7 +417,7 @@ class FccController(BaseController):
         else:
            file = request.POST.get('file',False)
         if post.message:
-           if len(post.message) <= 64000:
+           if len(post.message) <= 20000:
                parser = WakabaParser()
                post.message = parser.parseWakaba(post.message,self)     
            else:

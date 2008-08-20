c93f95c884d292329634abbb0dabf0c35e9e456a
johan <johan@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-08-20 16:56:14 +0000

    git-svn-id: svn://localhost/orphereus/trunk@398 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/controllers/fcp.py
+++ fc/controllers/fcp.py
@@ -39,7 +39,7 @@ class FcpController(OrphieBaseController):
                     rickroll = False
                 
             if (rickroll):
-                addLogEntry(LOG_EVENT_RICKROLLD, "Request rickrolld. Referer: %s, IP: %s" % (ref, request.environ["REMOTE_ADDR"]))                
+                addLogEntry(LOG_EVENT_RICKROLLD, "Request rickrolld. Referer: %s, IP: %s, User-Agent: %s" % (ref, request.environ["REMOTE_ADDR"], request.headers.get('User-Agent', '?')))                
                 redirect_to(fakeLinks[random.randint(0, len(fakeLinks) - 1)])
         
     def login(self, user):

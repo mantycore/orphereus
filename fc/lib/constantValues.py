092715e058f7d45b668263e1c69b811ca41f8e22
johan <johan@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-08-29 11:55:18 +0000

    git-svn-id: svn://localhost/orphereus/trunk@407 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/lib/constantValues.py
+++ fc/lib/constantValues.py
@@ -2,7 +2,7 @@ import sys
 import os 
 
 hashSecret = 'paranoia' # We will hash it by sha512, so no need to have it huge
-appPath = sys.path[0]
+appPath = os.path.dirname(__file__) #sys.path[0]
 templPath= os.path.join(appPath, 'fc/templates/')
 uploadPath = os.path.join(appPath, 'fc/uploads/')
 #u#ploadPathWeb = '/uploads/'

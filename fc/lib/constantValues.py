eb97ebf0f03a886305a7c86ba2529ff45256ad2e
johan <johan@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-09-04 18:06:07 +0000

    git-svn-id: svn://localhost/orphereus/trunk@440 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/lib/constantValues.py
+++ fc/lib/constantValues.py
@@ -15,7 +15,7 @@ if devMode:
     filesPathWeb='http://wut.anoma.ch/img2/'
 refControlList = ['anoma.ch', 'anoma.li', 'localhost', '127.0.0.1']
 fakeLinks = ['http://www.youtube.com/watch?v=oHg5SJYRHA0', 'http://meatspin.com/', 'http://youtube.com/watch?v=Uqot33mczsw', 'http://youtube.com/watch?v=dZBU6WzBrX8']
-minPassLength=10
+minPassLength=12
 alertEmail='lamo@sms.megafonsib.ru'
 alertServer='smtp.gmail.com'
 alertPort=587

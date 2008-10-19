fb20aa479a02bfb260c8c4ab5af9c3cca96eea33
RusAnon <RusAnon@b60fb4c0-664f-0410-90ee-dbbb0efa5d05>
2008-10-19 11:16:12 +0000

    Made security stuff optional
    git-svn-id: svn://localhost/orphereus/trunk@635 b60fb4c0-664f-0410-90ee-dbbb0efa5d05

--- fc/lib/helpers.py
+++ fc/lib/helpers.py
@@ -7,13 +7,19 @@ from webhelpers import *
 from pylons import config
 
 def modLink(string, secid):
-    p1 = string[0:4]
-    p2 = string[4:8]
-    p3 = string[8:len(string)]
-    return p1 + str(secid) + p2 + p3
+    if g.OPT.secureLinks:
+        p1 = string[0:4]
+        p2 = string[4:8]
+        p3 = string[8:len(string)]
+        return p1 + str(secid) + p2 + p3
+    else:
+        return string
 
 def modMessage(message, user):  
-    gv = config['pylons.g']  
-    uval = gv.uniqueVals[user.uidNumber() % (len(gv.uniqueVals) - 1)]
-    return message.replace('[SECURITY:UNIQUE_VAL]', uval)
+    if g.OPT.secureText:
+        gv = config['pylons.g']  
+        uval = gv.uniqueVals[user.uidNumber() % (len(gv.uniqueVals) - 1)]
+        return message.replace('[SECURITY:UNIQUE_VAL]', uval)
+    else:
+        return message
 

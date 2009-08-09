c7ee194c7851a2abaa1c095053278687ed90d4c8
hedger <hedger@.(none)>
2009-08-09 05:05:29 +0400

    fixed quick reply form for secondary-ids posts

--- Orphereus/templates/wakaba/ui.js
+++ Orphereus/templates/wakaba/ui.js
@@ -229,7 +229,7 @@ YForm.prototype = {
         link = $(link)
         var m = link.attr("href").match(/\/(\d+)\D+(\d+)$/)
         if(!m){ //thread
-          m = ["", this.thread_for_link(link), link.html().match(/\d+/)[0]]
+          m = ["", this.thread_for_link(link), link.attr('href').match(/E(\d+)/)[1]]
         };
         this.form.parent().insertAfter($("#quickReplyNode" + m[2]))
         this.set_fields(m[1])

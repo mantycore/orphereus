5dc71890ddb70f2302e296935ed35a048de23cdd
hedger <hedger@.(none)>
2009-08-12 18:51:50 +0400

    explicit language determination for memcache key

--- Orphereus/lib/helpers.py
+++ Orphereus/lib/helpers.py
@@ -45,7 +45,7 @@ def doFastRender(post, thread, controller):
 def repliesProxy(thread, controller):
     g = config['pylons.app_globals']
     user = controller.userInst
-    keyPrefix = str('%s%s%s' %(g.OPT.cachePrefix, user.lang, int(user.hideLongComments or (c.count==1))))
+    keyPrefix = str('%s%s%s' %(g.OPT.cachePrefix, get_lang()[0], int(user.hideLongComments or (c.count==1))))
     log.debug(keyPrefix)
     postsDict = dict([(post.id, post) for post in thread.Replies])
     postsRender = mc.get_multi(postsDict.iterkeys(), key_prefix = keyPrefix)

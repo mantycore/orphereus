d6e2a109ae454e6b6b9f0ebc133d216ea6d22fbc
hedger <hedger@nanode.su>
2009-12-11 12:06:22 +0300

    fixes x2

--- Orphereus/controllers/Orphie_Maintenance.py
+++ Orphereus/controllers/Orphie_Maintenance.py
@@ -561,11 +561,11 @@ class MaintenancePlugin(BasePlugin, AbstractPostingHook, AbstractMenuProvider):
     # Implementing BasePlugin
     def initRoutes(self, map):
         map.connect('hsMaintenance', '/holySynod/service/{actid}',
-                    controller = 'Orphie_maintenance', action = 'mtnAction',
+                    controller = 'Orphie_Maintenance', action = 'mtnAction',
                     actid = '')
         """
         map.connect('hsMaintenance', '/holySynod/service/{actid}/{secid}',
-        controller = 'Orphie_maintenance', action = 'mtnAction',
+        controller = 'Orphie_Maintenance', action = 'mtnAction',
         actid = '',
         secid = '')
         """

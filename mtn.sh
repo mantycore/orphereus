7b7121d8f460158d2baca7796400eee3253022bc
unknown <root@.(none)>
2009-12-27 01:36:18 +0600

    updated maintenance script

--- mtn.sh
+++ mtn.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-RND=`date | md5sum | awk '{print $1}'`
-echo -n $RND > /var/py/orphie/fc/secid
-wget --output-document=/tmp/lastMaintenance.html http://anoma.ch/holySynod/service/all/$RND
+# Deprecated calls
+#RND=`date | md5sum | awk '{print $1}'`
+#echo -n $RND > /var/py/orphie/fc/secid
+#wget --output-document=/tmp/lastMaintenance.html http://anoma.ch/holySynod/service/all/$RND
+
+cd /opt/orphereus
+paster maintenance --config=prod.ini --path=. RunAllObligatory > /tmp/orphereus.maintenanct.log 2>&1
